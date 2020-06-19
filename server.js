3778df056dc8d2e2d3a1350cc80d90b42bd4426f
Moacir Amaro <moacirjgt@gmail.com>
2020-06-19 19:40:53 -0300

    Configurando nunjucks

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.set('view engine', 'html')
+server.use(express.static('public'))
+
+nunjucks.configure('views', {
+    express: server
+})
+
+server.get("/",(req, res)=>{
+    return res.render('about')
+})
+
+server.get("/portifolio",(req, res)=>{
+    return res.render('portifolio')
+})
+
+server.listen(5000, ()=>{
+    console.log('Server running')
+
+})
